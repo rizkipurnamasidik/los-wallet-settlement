f917770ff957d553878557c17433acf01094ff52
rizkipurnamasidik <rizki.purnama.sidik@dwidasa.com>
2025-10-04 21:51:58 +0700

    chore: initialize service-auth project skeleton

---
+++ service-auth/service-auth/src/test/java/org/rizki/fintech/service_auth/ServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rizki.fintech.service_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
